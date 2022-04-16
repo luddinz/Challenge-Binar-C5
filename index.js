58c4e2d87c9f9ac51b6bdf44fe925d4a40d47b5b
nugialindro <nugialindro@gmail.com>
2022-04-17 02:31:09 +0700

    Create game & game page

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const morgan = require("morgan");
+const gameRouter = require("./game");
 const app = express();
 
 const port = 8080;
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
@@ -11,6 +14,8 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.use("/game", gameRouter);
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
