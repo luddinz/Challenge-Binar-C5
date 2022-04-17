48cc769531c500696b0509a5a1159d893211b57a
nugialindro <nugialindro@gmail.com>
2022-04-17 15:12:57 +0700

    create routing for register & login

---
+++ data.js
@@ -0,0 +1,14 @@
+const data = [
+  {
+    id: 1,
+    username: "nugi",
+    password: "igun",
+  },
+  {
+    id: 2,
+    username: "farhan",
+    password: "farhandibalik",
+  },
+];
+
+module.exports = data;
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const gameRouter = require("./game");
+const accountRouter = require("./account");
+const users = require("./data");
 const app = express();
 
 const port = 8080;
@@ -9,13 +12,19 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+// app.get("/register", (req, res) => {
+//   res.render("register");
+// });
+
 app.use("/game", gameRouter);
+app.use("/account", accountRouter);
 
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
